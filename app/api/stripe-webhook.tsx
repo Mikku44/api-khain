04e172720ed7890ff40fe00296931d37b87834a6
Mikku44 <coverblack12345@gmail.com>
2025-10-22 22:24:37 +0700

    feat : update key

--- app/api/stripe-webhook.tsx
+++ app/api/stripe-webhook.tsx
@@ -76,7 +76,7 @@ export async function action({ request }: ActionFunctionArgs) {
             const secretKey = process.env.MY_SECRET_KEY!;
 
             const fileLink = encrypt
-                ? decodeWithSecret(encrypt, secretKey)
+                ? decodeWithSecret(encrypt, "jjk3344")
                 : "https://discord.gg/KuMVmcK3cC";
 
             const payload = JSON.stringify({
